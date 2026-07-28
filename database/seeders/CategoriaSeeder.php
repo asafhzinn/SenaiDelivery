86fe9209921013c298fdfa7e2e17b502c193bc79
asafhzinn <hudsonasaf080@gmail.com>
2026-07-28 11:05:41 -0300

    Criamos a model, migration e seeder da categoria

---
+++ database/seeders/CategoriaSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Categoria;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class CategoriaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        
+          Categoria::create([
+            'nome' => 'Lanches',
+            'descricao' => 'Lanches Diversos',
+            'ativo' => true,
+            'ordem_exibicao' => 1
+          ]);
+        
+          Categoria::create([
+            'nome' => 'Porções',
+            'descricao' => 'Porção diversas',
+            'ativo' => true,
+            'ordem_exibicao' => 2
+          ]);
+
+            Categoria::create([
+                'nome' => 'Bebidas',
+                'descricao' => 'Bebidas diversas',
+                'ativo' => true,
+                'ordem_exibicao' => 3
+            ]);
+    }
+}
